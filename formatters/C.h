9fdcfceee3ea05c590558f09a8dba792c8a89b66
Ilya Kolbin <iskolbin@gmail.com>
2019-07-24 22:03:22 +0300

    udp

--- formatters/C.h
+++ formatters/C.h
@@ -1,5 +1,5 @@
 static int imgpack_formatter_C(struct ImgPackContext *ctx, FILE *f) {
-	fprintf(f, "enum %sIds = {\n", ctx->name);
+	fprintf(f, "enum %sIds {\n", ctx->name);
 	for (int i = 0; i < ctx->imagesUsed; i++) {
 		char *image_path = ctx->imagePaths[i];
 		int point_pos, start_pos;
@@ -16,17 +16,41 @@ static int imgpack_formatter_C(struct ImgPackContext *ctx, FILE *f) {
 		fprintf(f, "\t%s%s = %d,\n", ctx->name, buffer, i);
 	}
 	fprintf(f, "};\n\n");
-	fprintf(f, "static const char * %sPaths[%d] = {\n", ctx->name, ctx->imagesUsed);
+
+	fprintf(f, "static const char *%sImage = \"%s\";\n\n", ctx->name, ctx->outputImagePath);
+
+	fprintf(f, "static const int %sFrame[%d][6] = {\n", ctx->name, ctx->imagesUsed);
 	for (int i = 0; i < ctx->imagesUsed; i++) {
-		fprintf(f, "\t\"%s\",\n", ctx->imagePaths[i]);
+		struct stbrp_rect frame = get_frame_rect(ctx, i);
+		fprintf(f, "\t{%d, %d, %d, %d},\n", frame.x, frame.y, frame.w, frame.h);
 	}
 	fprintf(f, "};\n\n");
 
-	fprintf(f, "static stbrp_rect %sRects[%d] = {\n", ctx->name, ctx->imagesUsed);
+	fprintf(f, "static const int %sSourceSize[%d][2] = {\n", ctx->name, ctx->imagesUsed);
 	for (int i = 0; i < ctx->imagesUsed; i++) {
-		struct stbrp_rect frame = get_frame_rect(ctx, i);
-		fprintf(f, "\t{%d, %d, %d, %d, %d},\n", i, frame.x, frame.y, frame.w, frame.h);
+		fprintf(f, "\t{%d, %d},\n", ctx->imageSourceW[i], ctx->imageSourceH[i]);
 	}
+	fprintf(f, "};\n");
+/*
+	fprintf(f, "static const int %sY[%d] = {", ctx->name, ctx->imagesUsed);
+	for (int i = 0; i < ctx->imagesUsed; i++) fprintf(f, "\t%d,\n", get_frame_rect(ctx, i).y);
+	fprintf(f, "};\n");
+
+	fprintf(f, "static const int %sW[%d] = {", ctx->name, ctx->imagesUsed);
+	for (int i = 0; i < ctx->imagesUsed; i++) fprintf(f, "%d,", get_frame_rect(ctx, i).w);
+	fprintf(f, "};\n");
+
+	fprintf(f, "static const int %sH[%d] = {", ctx->name, ctx->imagesUsed);
+	for (int i = 0; i < ctx->imagesUsed; i++) fprintf(f, "%d,", get_frame_rect(ctx, i).h);
+	fprintf(f, "};\n");
+	
+	fprintf(f, "static const int %sSourceW[%d] = {", ctx->name, ctx->imagesUsed);
+	for (int i = 0; i < ctx->imagesUsed; i++) fprintf(f, "%d,", ctx->imageSourceW[id]);
+	fprintf(f, "};\n");
+
+	fprintf(f, "static const int %sSourceH[%d] = {", ctx->name, ctx->imagesUsed);
+	for (int i = 0; i < ctx->imagesUsed; i++) fprintf(f, "%d,", ctx->imageSourceH[id]);
 	fprintf(f, "};");
+	*/
 	return 0;
 }
